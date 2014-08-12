699364152e4b660bd1f8572f4e141ef606bc59b7
Henry Algus <henry@jd.ee>
2014-08-13 02:58:55 +0300

    Fix bad formatting

--- smartpost-extension/code/local/Jd/Smartpost/Model/Carrier/Smartpost.php
+++ smartpost-extension/code/local/Jd/Smartpost/Model/Carrier/Smartpost.php
@@ -7,85 +7,78 @@
 * @copyright 2014 Henry A,gus. All rights reserved.
 *
 */
-
 class Jd_Smartpost_Model_Carrier_Smartpost extends Mage_Shipping_Model_Carrier_Abstract implements Mage_Shipping_Model_Carrier_Interface {
-
-      protected $_code = 'smartpost';
-
-
-      /**
-       * 
-       * @access public
-       * @return string
-       */
-      public function getFormBlock() {
-        return 'Jd_Smartpost_Block_Smartpost';
-      }
-
-  
-      /**
-         * Collect rates
-         *
-         * @param Mage_Shipping_Model_Rate_Request $data
-         * @return Mage_Shipping_Model_Rate_Result
-         */
-        public function collectRates(Mage_Shipping_Model_Rate_Request $request)
-        {
-            
-            if (!$this->getConfigFlag('active')) {
-                    return false;
-            }
-
-            $freeBoxes = 0;
-            $totalPrice = 0;
-            if ($request->getAllItems()) {
-                foreach ($request->getAllItems() as $item) {
-                    $totalPrice += $item->getCalculationPrice() * $item->getQty();
-                }
-            }
-
-            $this->setFreeBoxes(0);
-            $result = Mage::getModel('shipping/rate_result');
-            
-            $shippingPrice = $request->getPackageQty() * $this->getConfigData('price');
-            $shippingPrice = $this->getFinalPriceWithHandlingFee($shippingPrice);
-            
-            // if shipping price is not empty, lets load all the places.
-            
-            if ($shippingPrice !== "") {
-                
-                $boxesList = @file_get_contents('http://www.smartpost.ee/places.php');
-                
-                if(strlen($boxesList) > 10) {
-                    $boxesArray = unserialize($boxesList);
-                }                
-                if(is_array($boxesArray)) {
-                    foreach($boxesArray as $key => $box) {
-                        if($box['active'] == 1) {
-                               $boxList[$key] = $box['name'] ;
-                        }
-                    }
-                    
-                    sort($boxList,SORT_STRING);
-					// create rates
-                    foreach($boxList as $box) {                   
-                            $method = Mage::getModel('shipping/rate_result_method');
-                            $method->setCarrier($this->_code);
-                            $method->setCarrierTitle($this->getConfigData('name'));
-                            $method->setMethod($box);
-                            $method->setMethodTitle($box);
-                            $method->setPrice($shippingPrice);
-                            $method->setCost($shippingPrice);
-                            $result->append($method);                      
-                    }
-                }
-            }
-            return $result;
-        }
-
-        public function getAllowedMethods()
-        {
-            return array('smartpost'=>$this->getConfigData('name'));
-        }
-      
+	protected $_code = 'smartpost';
+	
+	/**
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getFormBlock() {
+		return 'Jd_Smartpost_Block_Smartpost';
+	}
+	
+	/**
+	 * Collect rates
+	 *
+	 * @param Mage_Shipping_Model_Rate_Request $data        	
+	 * @return Mage_Shipping_Model_Rate_Result
+	 */
+	public function collectRates(Mage_Shipping_Model_Rate_Request $request) {
+		if (! $this->getConfigFlag ( 'active' )) {
+			return false;
+		}
+		
+		$freeBoxes = 0;
+		$totalPrice = 0;
+		if ($request->getAllItems ()) {
+			foreach ( $request->getAllItems () as $item ) {
+				$totalPrice += $item->getCalculationPrice () * $item->getQty ();
+			}
+		}
+		
+		$this->setFreeBoxes ( 0 );
+		$result = Mage::getModel ( 'shipping/rate_result' );
+		
+		$shippingPrice = $request->getPackageQty () * $this->getConfigData ( 'price' );
+		$shippingPrice = $this->getFinalPriceWithHandlingFee ( $shippingPrice );
+		
+		// if shipping price is not empty, lets load all the places.
+		
+		if ($shippingPrice !== "") {
+			
+			$boxesList = @file_get_contents ( 'http://www.smartpost.ee/places.php' );
+			
+			if (strlen ( $boxesList ) > 10) {
+				$boxesArray = unserialize ( $boxesList );
+			}
+			if (is_array ( $boxesArray )) {
+				foreach ( $boxesArray as $key => $box ) {
+					if ($box ['active'] == 1) {
+						$boxList [$key] = $box ['name'];
+					}
+				}
+				
+				sort ( $boxList, SORT_STRING );
+				// create rates
+				foreach ( $boxList as $box ) {
+					$method = Mage::getModel ( 'shipping/rate_result_method' );
+					$method->setCarrier ( $this->_code );
+					$method->setCarrierTitle ( $this->getConfigData ( 'name' ) );
+					$method->setMethod ( $box );
+					$method->setMethodTitle ( $box );
+					$method->setPrice ( $shippingPrice );
+					$method->setCost ( $shippingPrice );
+					$result->append ( $method );
+				}
+			}
+		}
+		return $result;
+	}
+	public function getAllowedMethods() {
+		return array (
+				'smartpost' => $this->getConfigData ( 'name' ) 
+		);
+	}
 }
--- smartpost-extension/code/local/Jd/Smartpost/controllers/Adminhtml/SmartpostController.php
+++ smartpost-extension/code/local/Jd/Smartpost/controllers/Adminhtml/SmartpostController.php
@@ -6,117 +6,110 @@
 * @copyright 2014 Henry Algus. All rights reserved.
 *
 */
-
-class Jd_Smartpost_Adminhtml_SmartpostController extends Mage_Adminhtml_Controller_Action
-{
-    protected function _construct()
-    {
-        //$this->setUsedModuleName('Jd_Smartpost');
-    }
-
-    /**
-     * 
-     * @access public
-     * @return void
-     */
-    public function indexAction()
-    {
-		$this->_redirect('/');
-    }
-
+class Jd_Smartpost_Adminhtml_SmartpostController extends Mage_Adminhtml_Controller_Action {
+	protected function _construct() {
+		// $this->setUsedModuleName('Jd_Smartpost');
+	}
+	
 	/**
-	 * Exports order to SmartPost.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
-	public function exportAction()
-	{
-		if($this->getRequest()->isXmlHttpRequest()){
-			$orderId = $this->getRequest()->getParam('order_id');
-			$order = $this->getOrder($orderId);  
-			$result = array("saved"=>1);
-			// is order payed 
-	        if($order instanceof Mage_Sales_Model_Order && ($order->getState()==Mage_Sales_Model_Order::STATE_PROCESSING || $order->getState()==Mage_Sales_Model_Order::STATE_COMPLETE)){
-			
+	public function indexAction() {
+		$this->_redirect ( '/' );
+	}
+	
+	/**
+	 * Exports order to SmartPost.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function exportAction() {
+		if ($this->getRequest ()->isXmlHttpRequest ()) {
+			$orderId = $this->getRequest ()->getParam ( 'order_id' );
+			$order = $this->getOrder ( $orderId );
+			$result = array (
+					"saved" => 1 
+			);
+			// is order payed
+			if ($order instanceof Mage_Sales_Model_Order && ($order->getState () == Mage_Sales_Model_Order::STATE_PROCESSING || $order->getState () == Mage_Sales_Model_Order::STATE_COMPLETE)) {
 				
-				
-				$items = $order->getAllItems();
-				$name=array();
-				foreach ($items as $itemId => $item)
-				{
-				    $name[] = $item->getName();
+				$items = $order->getAllItems ();
+				$name = array ();
+				foreach ( $items as $itemId => $item ) {
+					$name [] = $item->getName ();
 				}
 				
 				// get configuration data and prepare for API-delivery
-			
-				$uri = Mage::getStoreConfig('carriers/smartpost/service_uri');
-				$username = Mage::getStoreConfig('carriers/smartpost/username');
-				$password = Mage::getStoreConfig('carriers/smartpost/password');
-
-
-				$shipping = $order->getShippingAddress();
-				$place = str_replace("SmartPOST - ","",$order->getShippingDescription());
+				
+				$uri = Mage::getStoreConfig ( 'carriers/smartpost/service_uri' );
+				$username = Mage::getStoreConfig ( 'carriers/smartpost/username' );
+				$password = Mage::getStoreConfig ( 'carriers/smartpost/password' );
+				
+				$shipping = $order->getShippingAddress ();
+				$place = str_replace ( "SmartPOST - ", "", $order->getShippingDescription () );
 				
 				// load all the boxes
 				
-	            $boxesList = @file_get_contents('http://www.smartpost.ee/places.php');
-
-	            if(strlen($boxesList) > 10) {
-	                    $boxesArray = unserialize($boxesList);
-	            }
+				$boxesList = @file_get_contents ( 'http://www.smartpost.ee/places.php' );
+				
+				if (strlen ( $boxesList ) > 10) {
+					$boxesArray = unserialize ( $boxesList );
+				}
 				
 				$placeId = 0;
 				
-				if(is_array($boxesArray)) {
-						foreach($boxesArray as $box) {
-							if($box['name'] == $place) {
-								$placeId = $box['place_id'];
-								break;
-							}
+				if (is_array ( $boxesArray )) {
+					foreach ( $boxesArray as $box ) {
+						if ($box ['name'] == $place) {
+							$placeId = $box ['place_id'];
+							break;
 						}
+					}
 				}
 				
 				// create SmartPost request while using order data
-							
-				$orderData = array(array(
-					"place_id"=>$placeId,
-					"content" => implode(",",$name),
-					"name" =>$shipping->getFirstname()." ".$shipping->getLastname(),
-					"phone" => $shipping->getTelephone(),
-					"email" => $shipping->getEmail(),
-					"cash" => "0",
-					"ref" =>$order->getRealOrderId(),
-					"lang" => "et"
-				));	
+				
+				$orderData = array (
+						array (
+								"place_id" => $placeId,
+								"content" => implode ( ",", $name ),
+								"name" => $shipping->getFirstname () . " " . $shipping->getLastname (),
+								"phone" => $shipping->getTelephone (),
+								"email" => $shipping->getEmail (),
+								"cash" => "0",
+								"ref" => $order->getRealOrderId (),
+								"lang" => "et" 
+						) 
+				);
 				
 				// Send request to the SmartPost server
 				
-				if($placeId > 0) {
-					$client = new Zend_Http_Client($uri);
-					$client->setAuth($username,$password);
-					$response = $client->setRawData(Mage::helper('core')->jsonEncode($orderData), 'application/json')->request('POST');	
-				
+				if ($placeId > 0) {
+					$client = new Zend_Http_Client ( $uri );
+					$client->setAuth ( $username, $password );
+					$response = $client->setRawData ( Mage::helper ( 'core' )->jsonEncode ( $orderData ), 'application/json' )->request ( 'POST' );
 					$comment = 'Order sent to Smartpost';
-					$order->addStatusHistoryComment($comment);
-					$order->save();							
+					$order->addStatusHistoryComment ( $comment );
+					$order->save ();
 				}
-				
-	        } else {
-			    $result['saved'] = 0;
-	        }
-			$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
+			} else {
+				$result ['saved'] = 0;
+			}
+			$this->getResponse ()->setBody ( Mage::helper ( 'core' )->jsonEncode ( $result ) );
 		}
 	}
 	
 	/**
 	 * Load order by ID
-	 * 
+	 *
 	 * @access public
-	 * @param mixed $orderId
+	 * @param mixed $orderId        	
 	 * @return sales/order
 	 */
 	public function getOrder($orderId) {
-        return Mage::getModel('sales/order')->load($orderId); 
-    }
+		return Mage::getModel ( 'sales/order' )->load ( $orderId );
+	}
 }
